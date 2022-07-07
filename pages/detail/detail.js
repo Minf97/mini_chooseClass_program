ea834e05ff80e52533255b2731a05d4460a539ff
Minf97 <287728237@qq.com>
2022-07-07 13:14:38 +0800

    detail完成

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -8,9 +8,11 @@ Page({
     title: '',
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
+  naviToChoose(e) {
+    wx.navigateTo({
+      url: 'url',
+    })
+  },
   onLoad(options) {
     console.log(options);
     this.setData({
