875b44e33aa3df1cc2baf97e03fe8da98490219c
Minf97 <287728237@qq.com>
2022-07-07 11:56:15 +0800

    first commit

---
+++ pages/index/index.js
@@ -0,0 +1,92 @@
+Page({
+
+  data: {
+    statusBarHeight: getApp().globalData.statusBarHeight,
+    lineHeight: getApp().globalData.lineHeight,
+    rectHeight: getApp().globalData.rectHeight,
+    windowHeight: getApp().globalData.windowHeight,
+    pixelRatio: getApp().globalData.pixelRatio,     // rpx 与 px 的转换比例
+
+    label: [
+      {
+        title: '美术书法',
+        children: [
+          {
+            title: '美学工作室',
+            naviUrl: ''
+          },
+          {
+            title: '创想大师班',
+            naviUrl: ''
+          },
+          {
+            title: '国际动漫A班',
+            naviUrl: ''
+          }
+        ]
+      },
+      {
+        title: '舞蹈',
+        children: [
+          {
+            title: '舞蹈1',
+            naviUrl: ''
+          },
+          {
+            title: '舞蹈2',
+            naviUrl: ''
+          },
+          {
+            title: '舞蹈3',
+            naviUrl: ''
+          }
+        ]
+      },
+      {
+        title: '阅读语言',
+        children: [
+          {
+            title: '舞蹈1',
+            naviUrl: ''
+          },
+          {
+            title: '舞蹈2',
+            naviUrl: ''
+          },
+          {
+            title: '舞蹈3',
+            naviUrl: ''
+          }
+        ]
+      },
+      {
+        title: '特色课',
+        children: [
+          {
+            title: '舞蹈1',
+            naviUrl: ''
+          },
+          {
+            title: '舞蹈2',
+            naviUrl: ''
+          },
+          {
+            title: '舞蹈3',
+            naviUrl: ''
+          }
+        ]
+      },]
+  },
+
+  onLoad: function (options) {
+
+  },
+  onReady: function () {
+
+  },
+
+  onShow: function () {
+
+  },
+
+})
